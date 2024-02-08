41a3bd11dc429d971489d8a8c80fea57ce0721d1
mehmetakifacar <120928249+mehmetakifacar@users.noreply.github.com>
2024-02-08 23:34:08 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GivenMask(Person person)
+        {
+
+            if (_applicantService.CheckPerson(person) == true)
+            {
+                Console.WriteLine("Sonuç : " + person.FirstName + " " + person.LastName + " " + "için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine("Sonuç : " + person.FirstName + " " + person.LastName + " " + "için maske verilemedi !");
+            }
+        }
+
+
+    }
+}
---
+++ Maske_Takip_Uygulaması/Program.cs
@@ -0,0 +1,37 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Maske_Takip_Uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.FirstName = "Akif";
+            person1.LastName = "GÜNER";
+            person1.DateOfBirth = 1993;
+            person1.NationalIdentity = 12345678912;
+
+            Person person2 = new Person();
+
+            person2.FirstName = "Cengiz";
+            person2.LastName = "BEYYAMAN";
+            person2.DateOfBirth = 1998;
+            person2.NationalIdentity = 98765432101;
+
+
+            Console.WriteLine("Sorgulama Yapılıyor...");
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GivenMask(person1);
+            pttManager.GivenMask(person2);
+
+            Console.ReadLine();
+          
+        }
+    }
+
+   
+}
